9624b317e04e3e9a6ba3dc080695302f70f1ccfb
Faith Kariuki <tcwamboi@gmail.com>
2024-09-06 23:48:06 +0300

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'dark brown', 'grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 8, 'F', 'black', 'sassy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sargeant Brown', 10, 'M', 'dark brown', 'brave', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'brown', 'brave', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'green', 'grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'blue', 'sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 18, 'F', 'purple', 'sassy', 1); 
